4601b2a9425a3866520ad546e7b7e52e8f339198
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2023-07-28 18:13:19 +0300

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // 'color-primary': 'var(--color-primary)',
+        // 'color-secondary': 'var(--color-secondary)',
+        // 'color-text': 'var(--color-text)',
+        // 'color-bg': 'var(--color-bg)',
+        // 'color-error': 'var(--color-error)',
+        // 'color-success': 'var(--color-success)',
       },
     },
+    screens: {
+      sm: '481px', // 640px default
+      md: '768px',
+      lg: '1025px', // 1024px default
+      xl: '1280px',
+      '2xl': '1600px', // 1536px default
+    },
+  },
+  corePlugins: {
+    container: false,
   },
   plugins: [],
-}
+};
